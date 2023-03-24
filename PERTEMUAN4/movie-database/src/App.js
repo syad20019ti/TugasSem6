839dd068e4e4bd4f43b465035f761ab31c81cd9e
syad20019ti <syad20019ti@student.nurulfikri.ac.id>
2023-03-24 16:31:15 +0700

    PERTEMUAN4 movie-database

---
+++ PERTEMUAN4/movie-database/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+//import Hello from './Components/Hello';
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+function App() {  
+  return (
+    <div>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ PERTEMUAN4/movie-database/src/Components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>Copyright @Sya 2023</p>    
+        </footer>
+    );
+}
+
+export default Footer;
