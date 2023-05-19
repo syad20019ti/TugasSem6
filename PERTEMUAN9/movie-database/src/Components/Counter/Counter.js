5dafe2b79971e7f8d27340ecdc8cad0158c266a1
syad20019ti <syad20019ti@student.nurulfikri.ac.id>
2023-05-19 22:44:32 +0700

    Pertemuan 9

---
+++ PERTEMUAN9/movie-database/src/Components/Counter/Counter.js
@@ -0,0 +1,18 @@
+import {useState} from "react";
+
+function Counter(){
+    const [result, setResult] = useState(0);
+
+    function handleClick(){
+        setResult(result + 1);
+    }
+
+    return (
+        <div>
+            <p>Result: {result}</p>
+            <button onClick={handleClick}>Add</button>
+        </div>
+    );
+}
+
+export default Counter;
